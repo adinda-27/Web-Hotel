cd08d3610f46cb757ac2c1a257312d27b29fe80a
adinda-27 <89755405+adinda-27@users.noreply.github.com>
2022-06-15 22:25:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+  $(document).ready(function() {
+   $("#owl-example").owlCarousel();
+  $('.listing-detail span').tooltip('hide');
+        $('.carousel').carousel({
+            interval: 3000
+        }); 
+        $('.carousel').carousel('cycle');
+ }); 
